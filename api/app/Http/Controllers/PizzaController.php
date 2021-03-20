770cb5e6fd84b3f10b716d1bbc5739b484895f28
Finn Tonekk <github@tonekk.de>
2021-03-20 16:51:26 +0100

    Add PizzaController methods

--- api/app/Http/Controllers/PizzaController.php
+++ api/app/Http/Controllers/PizzaController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-class ExampleController extends Controller
+use App\Models\Pizza;
+use Illuminate\Http\Request;
+
+class PizzaController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -14,5 +17,34 @@ class ExampleController extends Controller
         //
     }
 
-    //
+    public function index() {
+      return response()->json(Pizza::all());
+    }
+
+    public function show($id)
+    {
+      // return Pizza::findOrFail($id);
+      return response()->json(Pizza::find($id));
+    }
+
+    public function create(Request $request)
+    {
+      $pizza = Pizza::create($request->all());
+
+      return response()->json($pizza, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+      $pizza = Pizza::findOrFail($id);
+      $pizza->update($request->all());
+
+      return response()->json($pizza, 200);
+    }
+
+    public function delete($id)
+    {
+      Pizza::findOrFail($id)->delete();
+      return response('', 204);
+    }
 }
