5d2a240e0243d95e5307deaec7157d967acbb95a
Finn Tonekk <github@tonekk.de>
2021-03-20 16:50:35 +0100

    Add pizzas migration

---
+++ api/database/migrations/2021_03_20_100026_create_pizzas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePizzasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->float('price', $precision = 8, $scale = 2);
+            $table->set('properties', [
+              'vegan',
+              'vegetarian',
+              'glutenfree',
+              'spicy',
+              'sweet'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+    }
+}
