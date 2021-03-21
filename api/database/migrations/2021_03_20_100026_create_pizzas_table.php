0d09cbc78cf26b44bb6788c5fed3f162d8d2882c
Finn Tonekk <github@tonekk.de>
2021-03-21 11:10:26 +0100

    Add default value for properties to migration

--- api/database/migrations/2021_03_20_100026_create_pizzas_table.php
+++ api/database/migrations/2021_03_20_100026_create_pizzas_table.php
@@ -24,7 +24,7 @@ class CreatePizzasTable extends Migration
               'glutenfree',
               'spicy',
               'sweet'
-            ]);
+            ])->default('');
         });
     }
 
